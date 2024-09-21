595af689830df7c45e59c07805a5aba492be54d1
Tahfimul <victory.josh.josh24@gmail.com>
2024-09-21 14:45:00 -0400

    Imported svgs from Figma for screen share and camera, and added states and onClick callback to camera button and screen share button so that user can be notified when sharing screen and camera feed

--- components/CallBanner.tsx
+++ components/CallBanner.tsx
@@ -12,16 +12,19 @@ type props =
 const CallBanner = ({title}:props)=>
 {
     const [muted, setMuted] = useState<boolean>(true)
+    const [cameraOn, setCameraOn] = useState<boolean>(false)
+    const [screenSharing, setScreenSharing] = useState<boolean>(false)
 
     const toggleMuted = ()=>{setMuted(!muted)}
-
+    const toggleCamera = ()=>{setCameraOn(!cameraOn)}
+    const toggleScreenShare= ()=>{setScreenSharing(!screenSharing)}
     return (
         <div className="bg-ziste-blue max-w-sm rounded overflow-hidden shadow-lg">
             <div className="px-6 py-4">
                 <div className="text-ziste-hazel font-bold text-xl mb-2">{title}</div>
             </div>
             <div className="px-6 pt-4 pb-2">
-                <button onClick={toggleMuted} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                <button onClick={toggleMuted} className="h-12 inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
                     
                     {muted?
                     
@@ -42,8 +45,44 @@ const CallBanner = ({title}:props)=>
                     }
 
                 </button>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+
+                <button onClick={toggleCamera} className="h-12 inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    {
+                        cameraOn?
+                            
+                            <svg width="43" height="33" viewBox="0 0 43 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect x="2.5" y="2.5" width="38" height="28" stroke="#3D405B" stroke-width="5"/>
+                                <circle cx="21.5" cy="16.5" r="6" stroke="#3D405B" stroke-width="5"/>
+                            </svg>
+
+                            :                        
+                            
+                            <svg width="43" height="33" viewBox="0 0 43 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect x="2.5" y="2.5" width="38" height="28" stroke="#3D405B" stroke-width="5"/>
+                                <circle cx="21.5" cy="16.5" r="6" stroke="#3D405B" stroke-width="5"/>
+                                <path d="M39.5 4.5L4.5 29" stroke="#3D405B" stroke-width="5"/>
+                            </svg>
+
+
+                    }
+                </button>
+
+                <button onClick={toggleScreenShare} className="h-12 inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    {
+                        screenSharing?
+                            <svg width="43" height="33" viewBox="0 0 43 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect x="2.5" y="2.5" width="38" height="28" stroke="#3D405B" stroke-width="5"/>
+                                <path d="M29 16.5L17.75 24.7272L17.75 8.27276L29 16.5Z" fill="#3D405B"/>
+                            </svg>
+                            :                        
+                            <svg width="43" height="33" viewBox="0 0 43 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect x="2.5" y="2.5" width="38" height="28" stroke="#3D405B" stroke-width="5"/>
+                                <path d="M29 16.5L17.75 24.7272L17.75 8.27276L29 16.5Z" fill="#3D405B"/>
+                                <path d="M39 5.5L5 28.5" stroke="#3D405B" stroke-width="5"/>
+                            </svg>
+
+                    }
+                </button>
             </div>
         </div>
     )
