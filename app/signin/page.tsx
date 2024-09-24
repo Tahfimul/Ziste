66d4a81dc0d9e39250e5f4e7ef697ec843aa0fbd
alexaguirre25 <kevin.aguirre93@myhunter.cuny.edu>
2024-09-23 21:50:53 -0400

    Layout.tsx file properly fixed, was missing <html> <body> code.

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -15,7 +15,7 @@ export default function Signin() {
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             className="mx-auto h-10 w-auto"
-            alt="Ziste"
+
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
             Sign in to your account
