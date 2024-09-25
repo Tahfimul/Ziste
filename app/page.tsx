49ec130ead77dc4171a7ab08200f1a8fc216a4e8
alexaguirre25 <kevin.aguirre93@myhunter.cuny.edu>
2024-09-24 21:30:50 -0400

    New Google Authentication and updated NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,14 @@
-'use client';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import Navbar from '../components/Navbar';
-import { signOut, useSession } from 'next-auth/react';
-import { redirect } from 'next/navigation';
 
 
 export default function Home() {
-  const session = useSession({
-    required: true,
-    onUnauthenticated() {
-      redirect('/');
-    },
-  });
   return (
     <>
-   <Navbar />
-
-    <h1>Connect with academia.</h1>  
-    <div className="p-8">
-      <div className='text-white'>{session?.data?.user?.email }</div>
-      <button className='text-white' onClick={() => signOut()}>Logout</button>
-    </div> 
+    <Navbar />
+    <h1>Connect with academia.</h1>   
     </>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,15 @@
 /* sources: */
 /* 1. sitepoint.com/creating-a-navbar-in-react/ */
 
+"use client";
+
 import React from 'react';
 import Link from "next/link";
 import './Navbar.css';
+import { signIn, useSession, signOut } from "next-auth/react"; //Added for signin session with Google
 
 const Navbar = () => {
+  const { data: session } = useSession();
   return (
 
 <nav className="navbar">
@@ -28,9 +32,37 @@ const Navbar = () => {
       
     </ul>
   </div>
-  <div className="navbar-right">
-    <Link href = "/signin"> Sign In </Link>
-  </div>
+
+  {session?.user ? (
+        <div className="flex gap-x-2 items-center">
+          <Link href="/"> </Link>
+          <p>
+            {session.user.name} {session.user.email}
+          </p>
+          <img src={session.user.image}
+            alt=""
+            className="w-10 h-10 rounded-full cursor-pointer"
+          />
+          <button
+            onClick={async () => {
+              await signOut({
+                callbackUrl: "/",
+              })
+            }}
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <button
+          onClick={() => signIn()}
+          className="bg-sky-400 px-3 py-2 rounded"
+        >
+          Sign In
+        </button>
+      )}
+
+      
 </nav>
 );
 };
