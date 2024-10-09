02ac5e5aceb51b0c537d8a3357a25d996df54b93
Ruckshada Khan <ruckshadakhan@Ruckshadas-MacBook-Air.local>
2024-10-09 05:41:13 -0400

    Add routes to portal & register pages, successfully functioning navbar2 UI

---
+++ app/portal/page.tsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../../components/Navbar2";
+
+export default function Catalog() {
+    return (
+        <>
+        <Navbar/>
+        <h1 className="text-black text-3xl">portal</h1>
+        </>
+    );
+}
