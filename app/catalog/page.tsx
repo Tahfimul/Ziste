955b74dbfa9393a1da6d1f379a250321ae8e35db
Ruckshada Khan <ruckshadakhan@Ruckshadas-MacBook-Air.local>
2024-10-14 19:49:17 -0400

    Add filterbar to catalog page

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { Filterbar } from "@/components/filterbar";
 import GradientBorder from "@/components/GradientBorder";
 import { Navbar } from "@/components/Navbar2";
 import { Searchbar } from "@/components/SearchBar";
@@ -6,30 +7,16 @@ import { Searchbar } from "@/components/SearchBar";
 export default function Catalog() {
     return (
         <>
-        <header className="bg-white">
+        <section id="catalog" className="bg-white">
         <Navbar/>
         <div className="flex justify-center p-3">
             <h1 className="text-6xl text-black">Course Catalog</h1>
         </div>
         <Searchbar/>
-        <h1 className="flex my-2 ml-44 text-black font-medium">5 listings:</h1>
-        <div className="flex justify-center">
-            <GradientBorder className="w-7/12 h-1/2 rounded-40 gradient-animate">
-                <div className="flex bg-white h-1/2 rounded-40 shadow-lg">
-                    <h1 className="px-5 pt-1 text-lg text-black">
-                        Ethics & Morality in Poetry
-                    </h1>
-                    <h2 className="block px-2 py-1 rounded-1/4 bg-[#81B29A]">
-                        <span className="block px-3 text-lg text-black">
-                            Professor Lupin
-                        </span>
-                    </h2>
-                        
-                </div>
-            </GradientBorder>
+        <Filterbar/>
+        </section>
 
-        </div>
-        </header>
+        <h1 className="flex my-2 ml-44 text-black font-medium">5 listings:</h1>
         </>
 
     );
