0d4745267e62fd356981f3946284c6ec4b63e062
Tahfimul <victory.josh.josh24@gmail.com>
2024-10-13 23:00:25 -0400

    Make navbar show up in main page when user is logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 // app/page.tsx
-
+'use client'
 import App from '@/components/welcome'
 import Hero from '@/components/hero';
+import { useSession } from "next-auth/react";
+import { Navbar } from '@/components/Navbar2';
 export default function Home() {
+  const { data: session } = useSession();
   return (
     <>
+      {session?.user ? 
+        (<Navbar/>):        
+        (<></>)
+
+      }
       <App/>
       <Hero/>
     </>
