8c77177349e9e30841ed6fe935b919345fe70b40
Ruckshada Khan <ruckshadakhan@Ruckshadas-MacBook-Air.local>
2024-10-14 19:52:11 -0400

    Remove old filter buttons from SearchBar

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Filterbar } from "@/components/filterbar";
+import { Filterbar } from "@/components/FilterBar";
 import GradientBorder from "@/components/GradientBorder";
 import { Navbar } from "@/components/Navbar2";
 import { Searchbar } from "@/components/SearchBar";
@@ -8,12 +8,12 @@ export default function Catalog() {
     return (
         <>
         <section id="catalog" className="bg-white">
-        <Navbar/>
-        <div className="flex justify-center p-3">
-            <h1 className="text-6xl text-black">Course Catalog</h1>
-        </div>
-        <Searchbar/>
-        <Filterbar/>
+            <Navbar/>
+            <div className="flex justify-center p-3">
+                <h1 className="text-6xl text-black">Course Catalog</h1>
+            </div>
+            <Searchbar/>
+            <Filterbar/>
         </section>
 
         <h1 className="flex my-2 ml-44 text-black font-medium">5 listings:</h1>
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -9,14 +9,6 @@ export const Searchbar = () => {
                 placeholder="Search"/>
             </GradientBorder>
         </div>
-
-        <div className="flex justify-center items-center mx-60 gap-8">
-            <h1 className="text-md font-medium text-[black]">Filter:</h1>
-            <button className="px-4 py-2 rounded-full border-[#F2CC8F] border-2 text-md text-[#3D405B]">Subject</button>
-            <button className="px-4 py-2 rounded-full border-[#E07A5F] border-2 text-md text-[#3D405B]">Course Length</button>
-            <button className="px-4 py-2 rounded-full border-[#81B29A] border-2 text-md text-[#3D405B]">Price</button>
-            <button className="px-4 py-2 rounded-full border-[#6E739E] border-2 text-md text-[#3D405B]">Materials</button>
-        </div>
     
     </header>
     );
