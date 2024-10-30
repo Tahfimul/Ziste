b0bbdfa424bd65751899396bde2cc1bceaba02a6
Ruckshada Khan <ruckshadakhan@Ruckshadas-MacBook-Air.local>
2024-10-30 00:45:21 -0400

    Create demo payment page linked to catalog & specific course URL route

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,10 +5,10 @@ export const Searchbar = () => {
     return (
     <header>
         <div className="flex justify-center p-[1.2vw]">
-            <GradientBorder className="flex justify-between items-center w-[66vw] h-[10vh] rounded-full gradient-animate">
+            <GradientBorder className="flex justify-between items-center w-[66vw] h-[9vh] rounded-full gradient-animate">
                 <div className="flex justify-between w-full h-full rounded-full items-center bg-white relative">
                     <input
-                    className="w-[95%] py-[1.5vh] px-[2vw] border-none text-[#3D405B] text-[1.8vw] rounded-full outline-none focus:ring-0 placeholder-[#3D405B]"
+                    className="w-[95%] py-[1.3vh] px-[2vw] border-none text-[#3D405B] text-[1.8vw] rounded-full outline-none focus:ring-0 placeholder-[#3D405B]"
                     placeholder="Search"
                     />
                     <Search className="w-[4vw] h-[4vh] text-[#3D405B] absolute right-[1vw] top-1/2 transform -translate-y-1/2" />
