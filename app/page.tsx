f60d5cdedefc1cabadf3c7dd68e249676bcaa566
abdullahsal702 <88620119+abdullahsal702@users.noreply.github.com>
2024-10-31 03:32:58 -0400

    student registration done

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 // app/page.tsx
-'use client'
-import App from '@/components/welcome'
-import Hero from '@/components/hero';
+"use client";
+import App from "@/components/welcome";
+import Hero from "@/components/hero";
 
-import {Navbar} from '@/components/Navbar';
-import AuthContextProvider from '@/components/contexts/AuthContextProvider';
+import { Navbar } from "@/components/Navbar";
+import AuthContextProvider from "@/components/contexts/AuthContextProvider";
+
 export default function Home() {
-  return (
-    <AuthContextProvider>
-      
-        <Navbar/>
-        <App/>
-        <Hero/>
-    </AuthContextProvider>
-  );
+    return (
+        <AuthContextProvider>
+            <Navbar />
+            <App />
+            <Hero />
+        </AuthContextProvider>
+    );
 }
